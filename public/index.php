8eac6972fc5f8f4bb2a743bb06b4a7a755d51de8
Andre Leal <andre.leal1992@gmail.com>
2026-03-12 14:43:33 -0300

    descrição da alteração

--- public/index.php
+++ public/index.php
@@ -1,6 +1,17 @@
 <?php
 
-require "../core/Router.php";
-require "../routes/web.php";
+session_start();
 
-Router::run();
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if ($uri === '/' || $uri === '') {
+    echo "Fleet SaaS funcionando";
+    exit;
+}
+
+if ($uri === '/login') {
+    require __DIR__ . '/views/login.php';
+    exit;
+}
+
+echo "Página não encontrada";
