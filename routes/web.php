0dc39ec34d88df9d84b11f9e1685a9b56e6f3265
Andre Leal <andre.leal1992@gmail.com>
2026-03-12 11:13:55 -0300

    descrição da alteração

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,9 @@
 <?php
 
 Router::get('/', 'DashboardController@index');
+
 Router::get('/login', 'AuthController@login');
+Router::post('/login', 'AuthController@authenticate');
+
+Router::get('/vehicles', 'VehicleController@index');
+Router::get('/fuel', 'FuelController@index');
